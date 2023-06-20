29921a8c09f9fd3172cbc2a3e8d1463543a0015d
Montana Burr <bur18019@byui.edu>
2023-06-20 13:59:14 -0600

    can create chat session

--- main.ts
+++ main.ts
@@ -3,51 +3,43 @@
 // given the user's input, queries the AI for search terms
 // related to the user's legal case.
 import fetch from "node-fetch";
+import { URL } from "url";
 type User = {"id":string,"display_name":string,"auth_method":string}
-async function discordLogin()
+async function login()
 {
-    try
-    {
-        const resp = await fetch("https://projects.laion.ai/auth/callback/discord",{
-            headers: {"Accept":"application/json"}
-        });
-        const json = await resp.json();
-        console.log(JSON.stringify(json));
-    }
-    catch (e)
-    {
-        console.error("could not log in: " + e);
-    }
-}
-async function getSearchTerms(userInput: string)
-{
-    let headers = {
-        "Content-Type":"application/json",
-        "Accept":"application/json"
-    };
-    let body = JSON.stringify({
-  "type": "random",
-  "user": {
-    "id": "string",
-    "display_name": "string",
-    "auth_method": "discord"
-  },
-  "collective": false,
-  "lang": "string"
-});
-    let resp = await fetch("https://projects.laion.ai/api/v1/tasks/",{
-        headers: headers,
-        body:body
+    const url = new URL("http://localhost:8000/auth/login/debug");
+    url.searchParams.append("username","legal_search");
+    const resp = await fetch(url,{
+        headers: {"Accept":"application/json"}
     });
-    let json = await resp.json();
-    try
-    {
-        return (json as Array<{"generated_text":string}>)[0]["generated_text"];
-    }
-    catch (e)
-    {
-        console.error(e);
-    }
+
+    if (!resp.ok)
+        throw resp.statusText;
+    const json = await resp.json();
+    console.debug(JSON.stringify(json));
+    const accessToken: string = json["access_token"]["access_token"];
+    return accessToken;
+    
+}
+async function getSearchTerms(accessToken:string,userInput: string)
+{
+    const headers = {
+        "Content-Type":"application/json",
+        "Accept":"application/json",
+        "Authorization":"Bearer " + accessToken
+    };
+    const url = new URL("http://localhost:8000/chats");
+    const resp = await fetch(url,{
+        headers: headers,
+        method: "POST",
+        body: "{}"
+    });
+    const json = await resp.json();
+    if (!resp.ok)
+        throw resp.statusText + JSON.stringify(json);
+    
+    console.debug(json)
+    return (json as {"generated_text":string})["generated_text"];
 }
 
-discordLogin();
+login().then((token)=>getSearchTerms(token,"police use of drug dogs")).then((val)=>console.log(val));
